ec6936f8060a9bef2edd2ab9df3016d5d80efb46
XynoxGaming <97520680+XynoxGaming@users.noreply.github.com>
2024-02-11 16:28:26 +0545

    Update index.js

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.listen(port, () => {
 });
 
 
-const statusMessages = ["Watching Netflix","Listening to Spotify","Playing GTA VI"];
+const statusMessages = ["Overseering Customer Support"];
 
 
 let currentIndex = 0;
